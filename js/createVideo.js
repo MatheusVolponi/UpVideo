a227a262c34f41387f57c24887e2786c870e0907
MatheusVolponi <matheusponath20@gmail.com>
2025-10-20 14:36:00 -0300

    feat: Criação do método POST para criação de novos vídeos na plataforma.

---
+++ js/createVideo.js
@@ -0,0 +1,18 @@
+import { apiConnect } from "./apiConnect.js";
+
+const form = document.querySelector("[data-form]");
+
+async function createVideo(event) {
+    event.preventDefault();
+
+    const image = document.querySelector("[data-image]").value;
+    const title = document.querySelector("[data-title]").value;
+    const url = document.querySelector("[data-url]").value;
+    const description = Math.floor(Math.random() * 10).toString();
+
+    await apiConnect.createVideo(title, description, url, image);
+
+    window.location.href = "../pages/uploaded-video.html";
+}
+
+form.addEventListener("submit", event => createVideo(event));
